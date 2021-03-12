6b816334ae404303fa1d72ead1ad7085fc243a25
caohaitao.cc <caohaitao.cc@bytedance.com>
2021-03-12 18:38:30 +0800

    feat: add reversLinkList

--- 链表/翻转链表.js
+++ 链表/翻转链表.js
@@ -42,4 +42,55 @@ for (var i = 0; i < 4; i++) {
 }
 
 
-console.log(reverseLinkList(list))
+// console.log(reverseLinkList(list))
+
+
+// 从指定区间翻转 left - right
+function reverseLinkListPart(head, left, right){
+
+		if (head===null || head.next===null) {
+			return head
+		}
+	
+		let node = new ListNode(-1,head);
+	
+		let n = 1;
+	
+		let pre = node;
+	
+		while(n<left){
+			pre = pre.next;
+			n++;
+		}
+	
+		const delta = right - left;
+	
+		let count = 0;
+	
+		/*
+			curr：指向待反转区域的第一个节点 left；
+			next：永远指向 curr 的下一个节点，循环过程中，curr 变化以后 next 会变化；
+			pre：永远指向待反转区域的第一个节点 left 的前一个节点，在循环过程中不变。
+		*/
+		let cur = pre.next;
+	
+	
+		while(count < delta){
+	
+			//  看不懂在纸上画一下
+			let next = cur.next;  // 记录当前节点的下一个节点
+	
+			cur.next = next.next; // 当前指向下一个节点
+	
+			next.next = pre.next; // 
+	
+			pre.next = next; // 
+	
+			count++
+	
+		}
+	
+		return node.next
+}
+
+// console.log(reverseLinkListPart(list))

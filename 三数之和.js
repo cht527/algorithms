7b71e9037255105143ceddcc2fdc72ab5b3173f1
caohaitao.cc <caohaitao.cc@bytedance.com>
2021-03-24 17:30:00 +0800

    chore: add threeSumCloset

--- 三数之和.js
+++ 三数之和.js
@@ -47,3 +47,56 @@ function three(arr,target) {
 const testa=[-1, 0, 1, 2, -1, -4];
 
 console.log(three(testa,0))
+
+/*
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。
+返回这三个数的和。假定每组输入只存在唯一答案。
+*/
+
+function threeSumClosest(nums,target){
+	nums.sort((a,b)=>a-b);
+
+	const len = nums.length;
+
+	let result = Number.MAX_SAFE_INTEGER;
+
+	for (let i = 0; i < len; i++) {
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+		let left=i+1;
+		let right=len-1;
+		while(left<right){
+			let sum = nums[i] + nums[left] + nums[right];
+			if(sum === target){
+				return target
+			}
+
+			if(Math.abs(sum - target)<Math.abs(result-target)){
+				result = sum
+			}
+
+			if(sum>target){
+					// 下面逻辑用于优化，实际可以直接 right--
+				    let _right = right - 1;
+                    // 移动到下一个不相等的元素
+                    while (left < _right && nums[_right] == nums[right]) {
+                        _right--;
+                    }
+                    right = _right;
+			}else{
+				// 下面逻辑用于优化，实际可以直接 left++
+
+				let _left = left + 1;
+                    // 移动到下一个不相等的元素
+                    while (right > _left && nums[_left] == nums[left]) {
+                        _left++;
+                    }
+                    left = _left;
+			}
+		}
+		
+	}
+
+	return result
+}

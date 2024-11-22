5fe37eb09111a802ee75cda790ebfb75f1751563
cht527 <cht527@126.com>
2024-11-22 22:05:50 +0800

    opt: factorial

--- 阶乘/factorial.js
+++ 阶乘/factorial.js
@@ -3,6 +3,16 @@ function factorialRecursive(number) {
   return number > 1 ? number * factorialRecursive(number - 1) : 1;
 }
 
+// 尾递归优化
+
+function factorialRecursiveOpt(number, total=1) {
+  if(number === 1){
+    return total
+  }
+
+  return factorialRecursiveOpt(number-1, number * total)
+}
+
 //--方法二
 
 function fac(n){
